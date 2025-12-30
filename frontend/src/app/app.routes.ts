4f61ae4aa094d3de103da58723433cfcb8b97260
Ihar Maiseyeu <horssupport@gmail.com>
2025-12-30 12:38:39 +0100

    Remove unnecessary pages

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,4 @@
 import { Routes } from '@angular/router';
-import { IngredientFormComponent } from './components/ingredient-form/ingredient-form.component';
-import { InventoryTableComponent } from './components/inventory-table/inventory-table.component';
 import { FermentableListComponent } from './components/fermentable-list/fermentable-list.component';
 import { FermentableFormComponent } from './components/fermentable-form/fermentable-form.component';
 import { HopListComponent } from './components/hop-list/hop-list.component';
@@ -11,9 +9,7 @@ import { MiscListComponent } from './components/misc-list/misc-list.component';
 import { MiscFormComponent } from './components/misc-form/misc-form.component';
 
 export const routes: Routes = [
-  { path: '', component: InventoryTableComponent },
-  { path: 'add', component: IngredientFormComponent },
-  { path: 'edit/:id', component: IngredientFormComponent },
+  { path: '', component: FermentableListComponent },
   { path: 'fermentables', component: FermentableListComponent },
   { path: 'fermentables/add', component: FermentableFormComponent },
   { path: 'fermentables/edit/:id', component: FermentableFormComponent },
